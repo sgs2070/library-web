15343a3717dd68dec38d227e3e42b9f93f66bf15
sgs2070 <121478473+sgs2070@users.noreply.github.com>
2023-05-05 15:13:47 +0300

    Add files via upload

---
+++ project/in.js
@@ -0,0 +1,82 @@
+/*var textOverImages=document.querySelectorAll(".products .box-container .box .content h3 div");
+for(var i=0;i<textOverImages.length;i++){
+    textOverImages[i].onclick=function(){
+        this.classList.toggle("show");
+    }
+}
+function showStuff() {
+    let hidden = document.getElementById('hidden');
+    if (hidden.style.display == "none") {
+      hidden.style.display = "block"
+    } else {
+      hidden.style.display = "none"
+    }
+  }
+  */
+  /*<img class="thumb" data-ord="1" data-text="my text" onclick='changeMainImg(this)' src="full/01.jpg">
+  function changeMainImg1(that) {
+  
+      document.getElementById("fullview").innerHTML = that.dataset.text;
+  }
+  /*
+ let myImage = document.getElementById('img1');
+ myImage.onclick = function(){
+
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc==="C:\Users\HP\Downloads\jarir1ed.jpg"){
+        myImage.setAttribute('src',"C:\Users\HP\Downloads\CONTACT.jpg")
+    }
+    else{
+        myImage.setAttribute('src',"C:\Users\HP\Downloads\jarir1ed.jpg")
+    }
+
+    var f = document.getElementById('img1');
+var x="good moring";
+f.onclick = x;
+  function my(){
+    document.getElementById('img1').classList.toggle("show");
+  }
+
+  document.getElementById("mybitn").onclick = function(){my()};
+function my(){
+    document.getElementById("myDrom").classList.toggle("show");
+}
+  } */
+  
+  /*let img = document.querySelectorAll("img")
+  img.forEach(i=>{
+    i.addEventListener("click",()=>{
+        let data= document.querySelectorAll('.${i.id}')[0]
+        data.style.display ="block"
+    })
+
+
+    let myImage = document.getElementById('img1');
+ myImage.onclick = function(){
+  var string1="Author:Manar El banawy";
+  var string2="classification:E-Books";
+   string3="No. of pages:220";
+
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc==="C:\Users\HP\Downloads\jarir1ed.jpg"){
+      document.getElementById("text1").innerHTML=string1,string2,string3;
+     }
+    else{
+        myImage.setAttribute('src',"C:\Users\HP\Downloads\jarir1ed.jpg")
+    }
+})*/
+
+document.getElementById("p1").innerHTML="hi book";
+ /*function info(){
+    let display = document.getElementById('img2');
+    var string1="Author:Manar El banawy";
+    const string2="classification:E-Books";
+    const string3="No. of pages:220";
+    
+    if(display.src.match('C:\Users\HP\Downloads\j2ed.png')){
+        var string9=prompt("Author:Manar El banawy");
+
+function myFunction(){
+  document.getElementById("content1").innerHTML="ii";
+    }
+  }*/
